1154785e500bef9e4bbbd86dc66f9f4e024c0f0b
Adarshkumar99 <56436370+Adarshkumar99@users.noreply.github.com>
2021-04-17 13:50:19 +0530

    Add files via upload

---
+++ storage/framework/views/0c82342c4e55c8fc0bd1a70746735533a35b5eae.php
@@ -0,0 +1,6 @@
+<div class="footer">
+	<div class="container">
+		<b class="copyright">&copy; <?php echo e(date('Y')); ?> - Library Management System </b> All rights reserved.
+	</div>
+</div>
+<?php /**PATH C:\xampp2\htdocs\Laravel-Library-Management-system-main\resources\views/layout/template_footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/1558764ce6f351cfff6f3b2068e3f95fd60921bb.php
@@ -0,0 +1,9 @@
+<tr>
+	<td><%= obj.id %></td>
+	<td><%= obj.book_issue_id %></td>
+	<td><%= obj.book_name %></td>
+	<td><%= obj.student_id %></td>
+	<td><%= obj.student_name %></td>
+	<td><%= obj.issued_at %></td>
+	<td><%= obj.return_time %></td>
+</tr><?php /**PATH C:\xampp2\htdocs\Laravel-Library-Management-system-main\resources\views/underscore/all_logs_display.blade.php ENDPATH**/ ?>
